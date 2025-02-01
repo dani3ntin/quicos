c13c0ca17837348d502e8cf82b9600f3b99b6272
Daniele Trentin <79662490+dani3ntin@users.noreply.github.com>
2025-02-01 17:37:06 +0100

    Update change_route.sh
    Edited change_route.sh: now it will work in s1 and b1

--- L23ConfigJob/files/change_route.sh
+++ L23ConfigJob/files/change_route.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
 CONFIG_FILE="/opt/openbach/scripts/curr_route.conf"
-vms=("c1" "s1")
-cmd_sat=("sudo ip route change 10.0.40.0/24 via 10.0.100.1" "sudo ip route change 10.0.10.0/24 via 10.0.40.254")
-cmd_terr=("sudo ip route change 10.0.40.0/24 via 10.0.30.1" "sudo ip route change 10.0.10.0/24 via 10.0.40.1")
+HOSTNAME=$(hostname)
+
+if [ "$HOSTNAME" == "b1" ]; then
+    cmd_sat="sudo ip route change 10.0.40.0/24 via 10.0.100.1"
+    cmd_terr="sudo ip route change 10.0.40.0/24 via 10.0.30.1"
+elif [ "$HOSTNAME" == "s1" ]; then
+    cmd_sat="sudo ip route change 10.0.10.0/24 via 10.0.40.254"
+    cmd_terr="sudo ip route change 10.0.10.0/24 via 10.0.40.1"
+else
+    echo "Error: script can only be executed on b1 or s1."
+    exit 1
+fi
+
 curr_route=$(head -n 1 "$CONFIG_FILE")
 echo "Current route: $curr_route"
 
-if [ "$curr_route" == "sat" ]
-then
-    echo "switching from sat to terr communication"
-    for i in "${!vms[@]}"; do
-        ( vagrant ssh ${vms[i]} -c "${cmd_terr[i]}" ) &
-    done
-else
-    echo "switching from terr to sat communication"
-    for i in "${!vms[@]}"; do
-        ( vagrant ssh ${vms[i]} -c "${cmd_sat[i]}" ) &
-    done
-fi
-
+# Switch della rotta
 if [ "$curr_route" == "sat" ]; then
+    echo "Switching from sat to terr communication"
+    $cmd_terr
     echo "terr" > "$CONFIG_FILE"
 else
+    echo "Switching from terr to sat communication"
+    $cmd_sat
     echo "sat" > "$CONFIG_FILE"
 fi
 
 wait
 echo All subshells finished
-
